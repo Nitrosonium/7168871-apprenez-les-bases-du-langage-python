697d84a5641bb1deb17f67f6f6d4123650914a92
Nitrosonium <cousin_lea@outlook.com>
2024-08-15 12:30:54 +0200

    Update main.py

--- P2/P2C2/énoncé/main.py
+++ P2/P2C2/énoncé/main.py
@@ -1,6 +1,34 @@
 def main():
     # Ecrivez votre code ici !
     # Attention tout votre code doit être indenté comme ce commentaire
+liste = input("Saisissez une liste de nombres séparés par des virgules :")
+liste.split(",")
+print("Liste des nombre", liste)
+
+
+somme=0
+for nombre in liste:
+    somme += int(nombre)
+    print("Somme des nombres : ", somme)
+
+moyenne = somme/len(liste)
+print("La moyenne des nombres : ", moyenne)
+
+for nombre in liste:
+    if int(nombre) > moyenne:
+        nombre_sup_moyenne +=1
+    print("Les nombres supérieurs à la moyenne sont : ", nombre_sup_moyenne)
+
+nombre_pair = 0
+i = 0
+while i < len(liste):
+    if int(list[i]) % 2 == 0:
+        nombre_pair +=1
+    i +=1
+print("Nombre de nombres pairs : ", nombre-pair)
+
+    
+ 
 
 
 # Ne touchez pas le code ci-dessous
